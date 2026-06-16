0cf3f03d77e92bbd045562accc0d3f75b1b86d0e
Renno <renoardy76@gmail.com>
2026-06-16 10:51:51 +0700

    Menyiapkan deploy railway

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# build.sh — dijalankan Render saat deploy
+# build.sh — dijalankan Railway saat deploy
 
-set -o errexit  # stop jika ada error
+set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --no-input
